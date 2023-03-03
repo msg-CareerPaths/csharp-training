9b0e46fbccf5991a16954c2009d316fd647a6d00
Bogdan Ghete <bogdan_ghetze@yahoo.com>
2023-03-03 13:51:23 +0200

    Update Product.cs

--- resources/code/startup_project/Models/Product.cs
+++ resources/code/startup_project/Models/Product.cs
@@ -8,11 +8,7 @@ namespace Products.Models
     {
         public int ID { get; set; }
         public string Name { get; set; } = null!;
-        public decimal Weight { get; set; }
 
-        public decimal Price { get; set; }
-
-        public string Description { get; set; } = null!;
         public override string ToString()
         {
             return JsonSerializer.Serialize(this);
